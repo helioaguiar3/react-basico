1d80cb567aa981256336cdcc942abb06d36e0a88
Helioaguiar3 <61293387+helioaguiar3@users.noreply.github.com>
2020-07-23 14:17:41 -0300

    Construindo componenetes.

---
+++ src/components/Comentario.js
@@ -0,0 +1,14 @@
+//importa o react que esta na pasta node
+import React from 'react';
+import './Comentario.css'
+
+//criar o componente escrito em JSX
+const Comentario = () => (
+    <div className="Comentario">
+        <div>Joao:</div>
+        <div>Olá, tudo bem?</div>
+    </div>
+
+);
+
+export default Comentario;

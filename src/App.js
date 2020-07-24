c8f4d90cc721601c5a53df24f814c41f4bb2ca9c
Helioaguiar3 <61293387+helioaguiar3@users.noreply.github.com>
2020-07-24 10:21:21 -0300

    Criação do state no App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 import Comentario from './components/Comentario';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu Projeto</h1>
+class App extends Component {
 
-      <Comentario name="João" email="joao@mail.com" data={new Date(2020,3,19)}>
-        Olá, tudo bem?
-      </Comentario>
-      
-      <Comentario name="Maria" email="maria@mail.com" data={new Date(2020,3,19)} >
-        Olá, tudo bem sim
-      </Comentario>
-    </div>
-  )
+  state = {
+    comentarios: [
+      {
+       nome: 'João',
+       email: 'joao@mail.com',
+       data: new Date(2020, 3, 19),
+       mensagem: 'Olá, tudo bem?'
+      },      
+      {
+        nome: 'Pedro',
+        email: 'pedro@mail.com',
+        data: new Date(2020, 3, 22),
+        mensagem: 'Olá, Tudo bem sim...'
+      }
+    ]
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <h1>Meu Projeto</h1>
+
+        {this.state.comentarios.map((comentario, indice )=> (
+        <Comentario 
+        key={indice}
+        nome={comentario.nome}
+        email={comentario.email}
+        data={comentario.data}>
+        {comentario.mensagem}
+        </Comentario>
+        ))}
+
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,16 +1,19 @@
 //importa o react que esta na pasta node
 import React from 'react';
-import './Comentario.css'
+import './Comentario.css';
 
 //criar o componente escrito em JSX
-const Comentario = props => (
+const Comentario = props => {
+
+
+    return (
     <div className="Comentario">
         <h2>{props.nome}</h2>
         <p>{props.email}</p>
         <div>{props.children}</div>
         <p>{props.data.toString()}</p>
     </div>
-
-);
+    )
+};
 
 export default Comentario;
